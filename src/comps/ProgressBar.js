642c4bec5a98d9d0ee8588df1bef66790dfbec8c
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-29 14:07:44 +0530

    basic progress

---
+++ src/comps/ProgressBar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import useStorage from '../hooks/useStorage';
+
+export default function ProgressBar({ file, setFile }) {
+    return (
+        <div className="progress-bar">Progress</div>
+    )
+}
