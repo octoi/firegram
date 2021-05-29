c1c0865cd5670991dd9e73c6178d180719ba1e62
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-29 14:48:25 +0530

    use firestore

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -7,7 +7,7 @@ const useFirestore = (collection) => {
     useEffect(() => {
         const unsub = projectFirestore.collection(collection).orderBy('createdAt', 'desc').onSnapshot((snap) => {
             let documents = [];
-            snap.forEach(doc => documents.push({ ...doc.data(), id: doc.id() }));
+            snap.forEach(doc => documents.push({ ...doc.data(), id: doc.id }));
             setDocs(documents);
         });
 
