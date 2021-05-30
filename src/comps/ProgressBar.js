58e5c456aeae0579a2a78357afb69a37d9659f00
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-30 06:40:02 +0530

    animated progress bar

--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import useStorage from '../hooks/useStorage';
+import { motion } from 'framer-motion';
 
 export default function ProgressBar({ file, setFile }) {
     const { url, progress } = useStorage(file);
@@ -11,6 +12,9 @@ export default function ProgressBar({ file, setFile }) {
     }, [url, setFile])
 
     return (
-        <div className="progress-bar" style={{ width: progress + '%' }}></div>
+        <motion.div className="progress-bar"
+            initial={{ width: 0 }}
+            animate={{ width: progress + '%' }}
+        />
     )
 }
