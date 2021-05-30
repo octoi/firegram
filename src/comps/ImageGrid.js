410cedaba80b670ab41b7c3987b3aed020977b1a
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-30 06:17:08 +0530

    updating selected image

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import useFirestore from '../hooks/useFirestore';
 
-export default function ImageGrid() {
+export default function ImageGrid({ setSelectedImg }) {
     const { docs } = useFirestore("images");
     console.log(docs)
 
     return (
         <div className="img-grid">
             {docs && docs.map(doc => (
-                <div key={doc.id} className="img-wrap">
+                <div key={doc.id} className="img-wrap" onClick={() => setSelectedImg(doc.url)}>
                     <img src={doc.url} alt={doc.id} />
                 </div>
             ))}
