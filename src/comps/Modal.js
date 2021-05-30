5718c23acaaa6d5df038ade7159882a74a6b4a66
fadhilsaheer <fadhilsaheer@gmail.com>
2021-05-30 06:25:46 +0530

    make it better

--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 export default function Modal({ selectedImg, setSelectedImg }) {
+
+    const handleClick = (e) => {
+        if (e.target.classList.contains('backdrop')) {
+            setSelectedImg(null)
+        }
+    }
+
     return (
-        <div className="backdrop" onClick={() => setSelectedImg(null)}>
+        <div className="backdrop" onClick={handleClick}>
             <img src={selectedImg} alt='enlarged pic' />
         </div>
     )
